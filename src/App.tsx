c873f04c1b7d9e3b2cf448cbdf7e1f69ab659930
YASH UMAK <patil.rtx@gmail.com>
2025-08-06 16:07:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const INITIAL_TWEETS: Tweet[] = [
       id: '1',
       name: 'Bhushan chambdi',
       username: 'Bhushan',
-      avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      avatar: 'https://static.vecteezy.com/system/resources/previews/010/056/184/large_2x/people-icon-sign-symbol-design-free-png.png',
     },
     timestamp: new Date(),
     likes: 142,
